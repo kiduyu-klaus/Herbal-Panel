f79390ac8659b4724972bd282899e3c3cbf4f064
kiduyu-klaus <muigakiduyu1995@gmail.com>
2020-08-18 12:33:56 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -4,6 +4,18 @@ include('include/config.php');
 include('include/checklogin.php');
 check_login();
 
+$qry_tips="SELECT COUNT(*) as num FROM herbs_tips";
+$total_tips= mysqli_fetch_array(mysqli_query($mysqli,$qry_tips));
+$total_tips = $total_tips['num'];
+
+$qry_herbs="SELECT COUNT(*) as num FROM herbs_herbs";
+$total_herbs = mysqli_fetch_array(mysqli_query($mysqli,$qry_herbs));
+$total_herbs = $total_herbs['num'];
+
+$qry_consultants="SELECT COUNT(*) as num FROM herbs_consultant";
+$total_consultants = mysqli_fetch_array(mysqli_query($mysqli,$qry_consultants));
+$total_consultants = $total_consultants['num'];
+
 ?>
 
 <div class="container-fluid">
@@ -11,11 +23,11 @@ check_login();
 	<div class="row">
 		<div class="col-md-3 col-sm-6 col-12">
 			<div class="info-box">
-				<span class="info-box-icon bg-info"><i class="fas fa-users"></i></span>
+				<span class="info-box-icon bg-info"><i class="fas fa-info"></i></span>
 
 				<div class="info-box-content">
-					<span class="info-box-text">Doctors</span>
-					<span class="info-box-number">24</span>
+					<span class="info-box-text">Herbal Tips</span>
+					<span class="info-box-number"><?php echo $total_tips;?></span>
 				</div>
 				<!-- /.info-box-content -->
 			</div>
@@ -27,8 +39,8 @@ check_login();
 				<span class="info-box-icon bg-success"><i class="fas fa-seedling"></i></i></span>
 
 				<div class="info-box-content">
-					<span class="info-box-text">Consultations</span>
-					<span class="info-box-number">410</span>
+					<span class="info-box-text">Herbs </span>
+					<span class="info-box-number"><?php echo $total_herbs;?></span>
 				</div>
 				<!-- /.info-box-content -->
 			</div>
@@ -37,11 +49,11 @@ check_login();
 		<!-- /.col -->
 		<div class="col-md-3 col-sm-6 col-12">
 			<div class="info-box">
-				<span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
+				<span class="info-box-icon bg-warning"><i class="fa fa-users"></i></span>
 
 				<div class="info-box-content">
-					<span class="info-box-text">Patients</span>
-					<span class="info-box-number">120</span>
+					<span class="info-box-text">Consultants</span>
+					<span class="info-box-number"><?php echo $total_consultants;?></span>
 				</div>
 				<!-- /.info-box-content -->
 			</div>
